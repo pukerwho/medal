20e7bc5dae332b76fecddf20cc247c25c0a04372
Vladimir Samojlov <v-moi@ya.ru>
2020-03-05 18:13:39 +0200

    edit page

--- page.php
+++ page.php
@@ -1,24 +1,12 @@
 <?php get_header(); ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<div class="container mx-auto px-4 lg:px-0 py-12">
-		<!-- Хлебные крошки -->
-		<div class="breadcrumbs mb-6">
-			<span typeof="v:Breadcrumb"> 
-				<a href="<?php echo home_url(); ?>" rel="v:url" property="v:title" class="my_yellow_color">
-					<?php _e( 'Главная', 's-cast' ); ?>
-				</a> 
-				› 
-			</span>
-			<span typeof="v:Breadcrumb"> <?php the_title(); ?>  </span>
-		</div>
-		<h1 class="title roboto-bold text-4xl uppercase mb-6">
+	<div class="container mx-auto px-4 lg:px-0">
+		<h1 class="with_line">
 			<?php the_title(); ?>	
 		</h1>
 		<div class="w-full">
-			<div class="my_bg_dark rounded-lg p-8">
-				<?php the_content(); ?>
-			</div>
+			<?php the_content(); ?>
 		</div>
 	</div>
 <?php endwhile; else: ?>
